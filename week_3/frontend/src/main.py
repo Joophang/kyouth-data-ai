e912c1507dbade2e848d9c4ddcb6f858de3644ed
Joo-Phang <joophang1023@gmail.com>
2026-05-26 23:25:21 +0200

    feat: implement chat page

--- week_3/frontend/src/main.py
+++ week_3/frontend/src/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="src/templates")
 
+load_dotenv()
+
 @app.get("/")
 def landing_page(request: Request):
+	backend_url = os.getenv("BACKEND_URL")
 	return templates.TemplateResponse(request=request, name="chat_page.html")
